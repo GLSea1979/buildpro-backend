20a02baacf168d7d67b0816848b3794bc14cb5bd
GLSea1979 <builtbygl@gmail.com>
2017-05-08 15:32:04 -0700

    built error middleware

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # yesterdays-travels-backend
+
+### Overview
+
+This RESTful API allows users to create an account and then inputs trips they have taken around the world. The trips are then displayable on a world map.
+
+# Current Version: 0.1.0
+
+# Architecture
+This app is built entirely with JavaScript and uses a MongoDB database.
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const app = new express();
+const mongoose = require('mongoose');
+const debug = require('debug')('yesterdays:server');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.load();
+
+// const errors = require('./lib/error-middleware.js');
+
 const PORT = process.env.PORT || 8000;
 
 app.use(express.static(`${__dirname}/build`));
 
-app.listen(PORT, () => {
-  console.log('the server is up on:', PORT);
-});
+app.listen(PORT, () => debug('the server is up on:', PORT));
