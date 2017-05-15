d2cb977bcd02d3282f50c521d97d57b78f92d3b6
GLSea1979 <builtbygl@gmail.com>
2017-05-15 14:14:24 -0700

    fixed typos in auth-route tests failing

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ const dotenv = require('dotenv');
 
 dotenv.load();
 
-// const errors = require('./lib/error-middleware.js');
+const errors = require('./lib/error-middleware.js');
+// const basic = require('./routes/')
+const authRouter = require('./routes/auth-route.js');
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(`${__dirname}/build`));
+app.use(cors());
+app.use(morgan('dev'));
+// app.use(basic);
+app.use(authRouter);
+app.use(errors);
 
 app.listen(PORT, () => debug('the server is up on:', PORT));
