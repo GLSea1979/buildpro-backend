1297a651ed2b224ac5c5b37593695d361e6dd1b9
GLSea1979 <builtbygl@gmail.com>
2017-05-17 10:08:10 -0700

    fixed  gitignore

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const dotenv = require('dotenv');
 dotenv.load();
 
 const errors = require('./lib/error-middleware.js');
-// const basic = require('./routes/')
 const authRouter = require('./routes/auth-route.js');
 
 const PORT = process.env.PORT || 8000;
@@ -20,7 +19,6 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
 app.use(morgan('dev'));
-// app.use(basic);
 app.use(authRouter);
 app.use(errors);
 
