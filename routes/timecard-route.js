707c2f6eae52cbd6743f625c99646b723ef92294
GLSea1979 <builtbygl@gmail.com>
2017-05-31 15:33:56 -0700

    added get timecard route and one test

--- routes/timecard-route.js
+++ routes/timecard-route.js
@@ -12,13 +12,24 @@ const Timecard = require('../model/timecard.js');
 const timecardRouter = module.exports = Router();
 
 timecardRouter.post('/api/employee/:employeeID/timecard', bearerAuth, jsonParser, function(req, res, next) {
-  debug('POST: /api/employee/:employeeID/timecard ---->', req.body.payPeriod);
+  debug('POST: /api/employee/:employeeID/timecard ---->', req.body);
 
   if(!req.body.payPeriod) return next(createError(400, 'pay period required'));
 
   return Timecard(req.body).save()
   .then( timecard => {
     res.json(timecard);
+  })
+  .catch(next);
+});
+
+timecardRouter.get('/api/timecard/:employeeID', bearerAuth, jsonParser, function(req, res, next) {
+  debug('GET: /api/timecard/:employeeID')
+
+  Timecard.find({employeeID: req.params.employeeID})
+  .then( timecards => {
+    if(!timecards) return next(createError(400, 'no timecards'));
+    res.json(timecards);
   })
   .catch(next);
 });
